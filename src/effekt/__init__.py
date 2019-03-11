e146c88c874151274c7617d4ab4c2a670a19e5f8
Emanuele Lillo <emalillo270304@gmail.com>
2019-03-11 18:20:17 +0100

    Add Config object, add crude TCP gateway

--- src/effekt/__init__.py
+++ src/effekt/__init__.py
@@ -3,14 +3,48 @@
 # This software is released under the MIT License.
 # https://opensource.org/licenses/MIT
 
-from functools import wraps
+from json import load, JSONDecodeError
 from .errors import EventNotFound, TooLowPriority
 from .ext.scratch import Scratch
+
+class ConfigObject:
+    def __init__(self, initialize={}):
+        self._config = initialize
+    
+    def set(self, key, value):
+        self._config[key] = value
+        return True
+
+    def get(self, key):
+        if key not in self._config:
+            return None
+
+        return self._config[key]
+    
+    def __getitem__(self, key):
+        return self.get(key)
+
+    def __setitem__(self, key, value):
+        return self.set(key, value)
+
+    def load(self, filename="config.json"):
+        try:
+            with open(filename, "r") as f:
+                try:
+                    new_config = load(f)
+                except JSONDecodeError:
+                    return None
+        except FileNotFoundError:
+            return None
+
+        self._config.update(new_config)
 
 class Effekt:
     def __init__(self, debug=False):
         # Initialize the listeners' dictionary
         self.listeners = {}
+
+        self.config = ConfigObject()
 
         """
         This dictionary has got a quite complex structure:
